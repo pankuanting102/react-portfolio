7cd21c2c0d6758b9d296ecf2826e1622a02d6c98
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2021-01-01 01:00:43 -0500

    try footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "./Footer.css"
 
 function Footer() {
     return (
-        <div>© 2020 All Right Reserved</div>
+        <footer>
+        <div className="p footer-right">© 2020 All Right Reserved</div>
+        </footer>
     );
 }
 
